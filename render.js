c32289b494dd5365e32ce327527dbaa67bc24994
Конышев Андрей Николаевич <konyshev.an@esphere.ru>
2021-09-19 18:17:06 +0300

    first render

--- render.js
+++ render.js
@@ -1,3 +1,7 @@
-function render() {
-    console.log('Привет!')
-}
+var asciidoctor = require('@asciidoctor/core')()
+var asciidoctorRevealjs = require('@asciidoctor/reveal.js')
+asciidoctorRevealjs.register()
+
+// Convert the document 'presentation.adoc' using the reveal.js converter
+var options = { safe: 'safe', backend: 'revealjs' }
+asciidoctor.convertFile('index.adoc', options)
